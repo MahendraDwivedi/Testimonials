53338b6a8230e186f05c86e9cf5c81678f59f745
mahendra_dwivedi <dwivedimahendra999912@gmail.com>
2024-02-19 16:21:42 +0530

    Made Changes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <div className="flex flex-col w-[100vw] h-[100vh] justify-center items-center bg-gradient-to-r from-blue-500 via-purple-500 to-pink-300 ...">
       <div className="text-center">
-        <h1 className="text-4xl font-bold">Our Testimonials</h1>
+        <h1 className="text-4xl font-bold">Our Achievements</h1>
         <div className="bg-black h-[4px] w-1/2 mt-1 mx-auto"></div>
       </div>
       <Testimonial reviews={reviews} />
